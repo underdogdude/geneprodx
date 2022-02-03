920c3921d2d67783ec10269ee5e10e5a68fa7b22
underdogdude <underdogbright@gmail.com>
2022-02-03 17:43:01 +0800

    Update #2 team template

--- functions.php
+++ functions.php
@@ -346,3 +346,23 @@ function acf_register_social_media() {
         )
     );
 }
+
+
+// Add Class to body
+add_filter( 'body_class', 'custom_body_class' );
+/**
+ * Add custom field body class(es) to the body classes.
+ *
+ * It accepts values from a per-page custom field, and only outputs when viewing a singular static Page.
+ *
+ * @param array $classes Existing body classes.
+ * @return array Amended body classes.
+ */
+function custom_body_class( array $classes ) {
+	// $new_class = is_page() ? get_post_meta( get_the_ID(), 'body_class', true ) : null;
+	$new_class = get_post_meta( get_the_ID(), 'body_class', true );
+	if ( $new_class ) {
+		$classes[] = $new_class;
+	}
+	return $classes;
+}
